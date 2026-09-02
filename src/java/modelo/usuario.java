5bd26713f8ccf387af9a1875c07d8b8aa37c3483
Estiven Ramirez <smestram@unicartagena.edu.co>
2026-09-02 16:12:34 -0500

    codigo de usuario.java

--- src/java/modelo/usuario.java
+++ src/java/modelo/usuario.java
@@ -11,4 +11,49 @@ package modelo;
  * @author ASUS
  */
 public class usuario {
+    private int id;
+    private String clave;
+    private String nombre;
+    private String correo;
+    private String rol;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
 }
