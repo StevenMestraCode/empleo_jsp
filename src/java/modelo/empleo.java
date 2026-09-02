58bfa7521e58c71b2d70445e16ebcbe5a73b44fb
Estiven Ramirez <smestram@unicartagena.edu.co>
2026-09-02 16:10:55 -0500

    codigo de empleo.java

--- src/java/modelo/empleo.java
+++ src/java/modelo/empleo.java
@@ -11,5 +11,85 @@ package modelo;
  * @author ASUS
  */
 public class empleo {
-    
+    private int id;
+    private String nombre;
+    private String categoria;
+    private String areaTrabajo;
+    private String empresa;
+    private String nivel;
+    private String sueldo;
+    private String funciones;
+    private String cargoJefe;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getAreaTrabajo() {
+        return areaTrabajo;
+    }
+
+    public void setAreaTrabajo(String areaTrabajo) {
+        this.areaTrabajo = areaTrabajo;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(String nivel) {
+        this.nivel = nivel;
+    }
+
+    public String getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(String sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public String getFunciones() {
+        return funciones;
+    }
+
+    public void setFunciones(String funciones) {
+        this.funciones = funciones;
+    }
+
+    public String getCargoJefe() {
+        return cargoJefe;
+    }
+
+    public void setCargoJefe(String cargoJefe) {
+        this.cargoJefe = cargoJefe;
+    }
 }
