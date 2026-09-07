a763852aaaf325c5beee09a897448db4011b77b2
Estiven Ramirez <smestram@unicartagena.edu.co>
2026-09-07 10:18:34 -0500

    cambios en getter and setters de ID

--- src/java/modelo/usuario.java
+++ src/java/modelo/usuario.java
@@ -11,20 +11,20 @@ package modelo;
  * @author ASUS
  */
 public class usuario {
-    private int id;
+    private String id;
     private String clave;
     private String nombre;
     private String correo;
     private String rol;
 
-    public int getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(String id) {
         this.id = id;
     }
-
+   
     public String getClave() {
         return clave;
     }
