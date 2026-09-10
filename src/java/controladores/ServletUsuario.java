5daed1d48faf61989067f1bc4b0d94c9fafbb58f
Estiven Ramirez <smestram@unicartagena.edu.co>
2026-09-10 16:28:40 -0500

    correciones en servletUsuario.java

--- src/java/controladores/ServletUsuario.java
+++ src/java/controladores/ServletUsuario.java
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import modelo.CRUDusuario;
 import modelo.usuario;
+import utilidades.EnviarCorreo;
 
 /**
  *
@@ -75,15 +76,44 @@ public class ServletUsuario extends HttpServlet {
             } else if(accion.equals("inicioSesion")){
                 CRUDusuario crud= new CRUDusuario();
                 usuario alguien = crud.inicioSesion(request.getParameter("correo"), request.getParameter("clave"));
-                request.getSession().setAttribute("usuario_inicioSesion", alguien);
+                request.getSession().setAttribute("usuario_login", alguien);
                 response.sendRedirect("index.jsp?mensaje=Bienvenido " + alguien.getNombre());
             }else if (accion.equals("listar")) {
                 CRUDusuario crud = new CRUDusuario();
                 usuario[] listado = crud.listarTodos();
                 request.getSession().setAttribute("usuario_listar", listado);
                 response.sendRedirect("web/usuario/listar.jsp");
+            } else if(accion.equals("listarPorRol")){
+                CRUDusuario crud = new CRUDusuario();
+                usuario [] listado = crud.listarPorRol(request.getParameter("rol"));
+                request.getSession().setAttribute("usuario_listarRol", listado);
+                response.sendRedirect("web/usuario/reporteUsuarioRol.jsp");
+            }else if(accion.equals("listarPorNombre")){
+                CRUDusuario crud = new CRUDusuario();
+                usuario [] listado = crud.listarPorNombre(request.getParameter("nombre"));
+                request.getSession().setAttribute("usuario_listarNombre", listado);
+                response.sendRedirect("web/usuario/reporteUsuariosNombre.jsp");
+            }else if (accion.equals("recuperarClave")) {
+    String correo = request.getParameter("correo");
+    CRUDusuario crud = new CRUDusuario();
+    usuario u = crud.buscarPorCorreo(correo);
 
-            } else {
+    if (u != null) {
+        // Generar token o usar la clave actual
+        String claveTemporal = u.getClave(); // o generar nueva clave aleatoria
+
+        // Enviar correo
+        try {
+            EnviarCorreo.enviar(correo, "Recuperación de clave",
+                "Tu clave actual es: " + claveTemporal);
+            response.sendRedirect("web/usuario/login.jsp?mensaje=Clave enviada al correo");
+        } catch (Exception e) {
+            response.sendRedirect("web/usuario/recuperarClave.jsp?mensaje=Error al enviar correo");
+        }
+    } else {
+        response.sendRedirect("web/usuario/recuperarClave.jsp?mensaje=Correo no registrado");
+    }
+}else {
                 response.sendRedirect("web/mensaje.jsp?mensaje=Acción no reconocida");
             }
 
