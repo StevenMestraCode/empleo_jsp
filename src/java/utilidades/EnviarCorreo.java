08adbe174c6fb91e8ec30bc64908530c5668e67d
Estiven Ramirez <smestram@unicartagena.edu.co>
2026-09-16 11:25:31 -0500

    Cambio en el puerto smtp

--- src/java/utilidades/EnviarCorreo.java
+++ src/java/utilidades/EnviarCorreo.java
@@ -37,7 +37,7 @@ public class EnviarCorreo {
         // Configuración de propiedades SMTP para Gmail
         Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.port", "465");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
 
